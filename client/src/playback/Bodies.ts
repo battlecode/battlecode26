b4e8bcbf6539cada1c5ced63e79163fd8dc5b5f4
Jerry Zhang <jerry.09.zhang@gmail.com>
2025-10-31 23:20:22 -0400

    focuses based off dropdown

--- client/src/playback/Bodies.ts
+++ client/src/playback/Bodies.ts
@@ -150,6 +150,7 @@ export default class Bodies {
         // multiSelectMode: boolean = false,
         selectedBodyID?: number,
         selectedBodyIDs?:  Array<number>,
+        focusedBodyIDs?:  Array<number>,
         hoveredTile?: Vector
     ): void {
         for (const body of this.bodies.values()) {
@@ -157,26 +158,11 @@ export default class Bodies {
                 body.draw(match, bodyCtx)
             }
 
-            // if (multiSelectMode) {
-            //     for(const selectedID of selectedBodyIDs ?? []) {
-            //         const selected = selectedID === body.id
-            //         const hovered = !!hoveredTile && vectorEq(body.pos, hoveredTile)
-            //         if (overlayCtx) {
-            //             body.drawOverlay(match, overlayCtx, config, selected, hovered)
-            //         }
-            //     }
-            // }
-            for(const selectedID of selectedBodyIDs ?? []) {
-                const selected = selectedID === body.id
-                const hovered = !!hoveredTile && vectorEq(body.pos, hoveredTile)
-                if (overlayCtx) {
-                    body.drawOverlay(match, overlayCtx, config, selected, hovered)
-                }
-            }
-            const selected = selectedBodyID === body.id
+            const selected = (selectedBodyID === body.id || !!selectedBodyIDs?.includes(body.id))
             const hovered = !!hoveredTile && vectorEq(body.pos, hoveredTile)
+            const focused = !!focusedBodyIDs?.includes(body.id)
             if (overlayCtx) {
-                body.drawOverlay(match, overlayCtx, config, selected, hovered)
+                body.drawOverlay(match, overlayCtx, config, selected && focused, hovered || (selected && !focused))
             }
         }
     }
